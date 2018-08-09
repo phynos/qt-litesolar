980257cc620d6dc0c1456656f5637f827a9fb752
phynos <phynos@126.com>
2018-08-09 23:28:33 +0800

    新增sqlite源码

--- src/make/MAKE.bat
+++ src/make/MAKE.bat
@@ -1,12 +1,22 @@
 @echo off
+color 0a
 
 :loop
-set /p target=请选择构建目标：0=所有；1=libmodbus；2=duktape;3=lua;4=libmodbus_lua
+echo 当前支持以下构建：
+echo 0=所有
+echo 1=libmodbus
+echo 2=duktape
+echo 3=lua
+echo 4=libmodbus_lua
+echo 5=sqlite3
+echo .  
+set /p target=请选择构建目标：
 if /i "%target%" == "0" goto all
 if /i "%target%" == "1" goto libmodbus
 if /i "%target%" == "2" goto duktape
 if /i "%target%" == "3" goto lua
 if /i "%target%" == "4" goto libmodbus_lua
+if /i "%target%" == "5" goto sqlite3
 echo 你必须选择一个目标 goto loop
 
 :all
@@ -32,6 +42,12 @@ goto end
 echo 开始构建libmodbus-lua动态库
 mingw32-make -f MAKE libmodbus_lua
 goto end
+
+:sqlite3
+echo 开始构建sqlite3动态库
+mingw32-make -f MAKE sqlite3
+goto end
+
 
 :end
 pause
