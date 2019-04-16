b58cbf46e15077a65f6452a8343b1322dddd8dfc
phynos <phynos@126.com>
2019-04-17 00:10:24 +0800

    dfd

--- src/make/MAKE.bat
+++ src/make/MAKE.bat
@@ -1,29 +1,47 @@
 @echo off
 color 0a
 
+mingw32-make -version
+If errorlevel 1 (
+    echo 没有安装mingw，请安装
+    goto end
+) Else (
+    echo 环境检测成功！
+	cls
+    call :showLogo                                                  
+)
+
 :loop
 echo 当前支持以下构建：
-echo 0=所有
-echo 1=libmodbus
-echo 2=duktape
-echo 3=lua
-echo 4=暂缺
-echo 5=sqlite3
-echo 21=lsqlite3
-echo 22=lmodbus
-echo 100=退出
+echo 0=清理
+echo 1=所有
+echo 2=sqlite3
+echo 3=duktape
+echo 4=lua
+echo 5=暂缺
+echo 101=libmodbus
+echo 201=lsqlite3
+echo 202=lmodbus
+echo 999=退出
 echo .  
 set /p target=请选择构建目标：
-if /i "%target%" == "0" goto all
-if /i "%target%" == "1" goto libmodbus
-if /i "%target%" == "2" goto duktape
-if /i "%target%" == "3" goto lua
-if /i "%target%" == "4" goto other
-if /i "%target%" == "5" goto sqlite3
-if /i "%target%" == "21" goto lsqlite3
-if /i "%target%" == "22" goto lmodbus
-if /i "%target%" == "100" goto end
+if /i "%target%" == "0" goto clean
+if /i "%target%" == "1" goto all
+if /i "%target%" == "2" goto sqlite3
+if /i "%target%" == "3" goto duktape
+if /i "%target%" == "4" goto lua
+if /i "%target%" == "5" goto other
+if /i "%target%" == "101" goto libmodbus
+if /i "%target%" == "201" goto lsqlite3
+if /i "%target%" == "202" goto lmodbus
+if /i "%target%" == "999" goto end
 echo 你必须选择一个目标 goto loop
+
+:clean
+cls
+echo 暂不支持
+echo:
+goto loop
 
 :all
 mingw32-make -f MAKE
@@ -81,6 +99,16 @@ goto loop
 :other
 echo 暂时不支持
 goto loop
+
+:showLogo
+echo:
+echo "  _       _   _            ____            _                    "
+echo " | |     (_) | |_    ___  / ___|    ___   | |   __ _   _ __     "
+echo " | |     | | | __|  / _ \ \___ \   / _ \  | |  / _` | | '__|    "
+echo " | |___  | | | |_  |  __/  ___) | | (_) | | | | (_| | | |       "
+echo " |_____| |_|  \__|  \___| |____/   \___/  |_|  \__,_| |_|       "
+echo:      
+goto :eof
 
 :end
 pause
