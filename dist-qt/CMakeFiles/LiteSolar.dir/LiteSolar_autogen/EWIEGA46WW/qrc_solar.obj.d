80430d8466e8aca1cd9c6ca135fcd82feca193c8
phynos <phynos@126.com>
2021-11-14 12:11:52 +0800

    d

--- dist-qt/CMakeFiles/LiteSolar.dir/LiteSolar_autogen/EWIEGA46WW/qrc_solar.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/LiteSolar.dir/LiteSolar_autogen/EWIEGA46WW/qrc_solar.obj: \
- D:\workspace-qt\qt-litesolar\dist-qt\LiteSolar_autogen\EWIEGA46WW\qrc_solar.cpp
--- dist-qt/CMakeFiles/LiteSolar.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LiteSolar.
-# This may be replaced when dependencies are built.
