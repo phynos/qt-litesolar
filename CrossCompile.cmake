5a1e8733344530342e6b41d0d1d9bb201049b204
phynos <phynos@126.com>
2022-05-18 05:50:46 +0000

    1

---
+++ CrossCompile.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(TOOLCHAIN_PATH /opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-g++)
+
